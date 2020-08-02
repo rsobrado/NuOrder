cbe86fee0db51de3a580432813232d18b37de620
Roger Sobrado <roger.sobrado.e@gmail.com>
2020-08-02 10:54:48 -0600

    github pages added

---
+++ docs/precache-manifest.298105a31981c33ab629d547c8819046.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96706423c6196b8e036d24993219bd56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "882cc655e437060ccfe5",
+    "url": "/static/css/main.9a35063e.chunk.css"
+  },
+  {
+    "revision": "15a23258cff4e796cb7d",
+    "url": "/static/js/2.9698d5e2.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.9698d5e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "882cc655e437060ccfe5",
+    "url": "/static/js/main.0f95a956.chunk.js"
+  },
+  {
+    "revision": "f918b6777655ef28a86b",
+    "url": "/static/js/runtime-main.866f21f5.js"
+  }
+]);
